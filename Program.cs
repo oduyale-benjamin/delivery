b72911c2bede2dc577c1b0d3b6827d593103c54c
oduyale-benjamin <oduyalebenjamin@gmail.com>
2026-09-24 16:18:44 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,32 +1,44 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using pizza.Models;
 using pizza.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllersWithViews();
-
-var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+builder.Services.AddDbContext<PizzaDbContext>(Options => Options.UseNpgsql(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+));
 
 builder.Services.AddDbContext<PizzaDbContext>(options =>
-    options.UseNpgsql(connectionString));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapStaticAssets();
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Pizza}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
 
 app.Run();
