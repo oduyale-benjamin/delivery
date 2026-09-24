a3848690dac952b6e29c40d16ab823981afd2b5d
oduyale-benjamin <oduyalebenjamin@gmail.com>
2026-09-24 15:57:55 +0100

    Refactor database connection and routing configuration

--- Program.cs
+++ Program.cs
@@ -1,44 +1,32 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using pizza.Models;
 using pizza.Data;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<PizzaDbContext>(Options => Options.UseNpgsql(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-));
+
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
 
 builder.Services.AddDbContext<PizzaDbContext>(options =>
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-    )
-);
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Pizza}/{action=Index}/{id?}");
 
 app.Run();
